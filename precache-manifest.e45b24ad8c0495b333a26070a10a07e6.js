776474ca225a791959463b7961383558e04c39c5
Gabriel <mail@gabrieldavison.me.uk>
2020-12-03 14:49:24 +0000

    Updates

---
+++ precache-manifest.e45b24ad8c0495b333a26070a10a07e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7af1ebd9feb03dfc23ac00a22183196",
+    "url": "/temper-client/index.html"
+  },
+  {
+    "revision": "0718b1ad3d25c3ccc067",
+    "url": "/temper-client/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "0718b1ad3d25c3ccc067",
+    "url": "/temper-client/static/js/2.4274a1de.chunk.js"
+  },
+  {
+    "revision": "b27f45f827318d07641988474663d078",
+    "url": "/temper-client/static/js/2.4274a1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea8b9aa4d494987faf48",
+    "url": "/temper-client/static/js/main.70672db5.chunk.js"
+  },
+  {
+    "revision": "88e952eed844af47aa55",
+    "url": "/temper-client/static/js/runtime-main.e8165ecb.js"
+  }
+]);
